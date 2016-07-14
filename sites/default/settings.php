7b91d8eb52d2e951a25c2fd53536521c20d9c1ae
vagrant <vagrant@homestead>
2016-07-14 23:48:01 +0000

    memcache

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -219,8 +219,8 @@ $databases = array (
     array (
       'database' => 'lucesycolores',
       'username' => 'root',
-      'password' => '',
-      'host' => '192.168.5.144',
+      'password' => 'root',
+      'host' => 'localhost', #192.168.5.144
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',
