7e9c1e82b11ea097675d58a52ffd98bb8c82af11
keylor <keylor@orbelink.com>
2016-08-16 19:34:31 -0400

    final dia

--- sites/all/themes/lucesycolores/templates/views/views-view-field--terapias-usuarios--page-1--body.tpl.php
+++ sites/all/themes/lucesycolores/templates/views/views-view-field--terapias-usuarios--page-1--body.tpl.php
@@ -23,9 +23,37 @@
  */
 ?>
 <?php 
+  global $base_url;
   $terapiaId = $row->{$field->field_alias};
-  $terapia = node_load($terapiaId);
-  //$body = field_get_items('node', $terapia, 'body');
+  $terapia = node_load($terapiaId);;
   $body = $terapia->body[LANGUAGE_NONE][0]['value'];
-  var_export($body);
+  $pattern = "/{[^}]*}/";
+  if(preg_match_all($pattern, $body, $resultados, PREG_SET_ORDER)){
+       $token = '';
+       $productoRef = '';
+       $urlAlias = '';
+       foreach ($resultados as $resultado){
+           //buscar el url alias de los token
+           $token = str_replace("{","",$resultado[0]);
+           $token = str_replace("}","",$token);
+           $token = preg_replace('/\s+/', '', $token);
+           $productoRef = explode(':', $token);
+           $producto = node_load($productoRef[0]);
+           $urlAlias = drupal_get_path_alias('node/'.$productoRef[0]);
+           //reemplazar el token con el url del producto
+           $body = str_replace($resultado,'<a href="'.$base_url.'/'.$urlAlias.'">'.$producto->title.'</a>', $body);
+       }
+   }
+   $body = '<p>'.$body.'</p>';
+   print $body;
+/*$varmensaje = '{mensaje-'.$form_num.'}';
+           $mensaje    = '<?php $this->mensaje(\''.$form_num.'\'); ?>';
+           if(strpos($html, $varmensaje)){
+               $html    = str_replace($varmensaje, $mensaje, $html);
+               $mensaje = '';
+           }
+           $form = '<form' . $resultado[1] . '>'; echo $form;
+           $hidden = '<input type="hidden" name="_form" value="'.$form_num.'"  />';
+           $html = str_replace($form, $mensaje.$form.$hidden, $html);*/
+
 ?>
