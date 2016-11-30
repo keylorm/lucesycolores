12a3a1eb7ae3e6afdce75d4983ff3489094c8d20
keylor <keylor@orbelink.com>
2016-11-30 10:38:24 -0500

    antes publicar

--- sites/all/themes/lucesycolores/templates/fields/field--field-info-libros.tpl.php
+++ sites/all/themes/lucesycolores/templates/fields/field--field-info-libros.tpl.php
@@ -10,10 +10,21 @@ $desc_caracteristicas = "";
       }
     }
 }
-
-echo '<div id="tabs">
+$nodo_id = arg(1);
+$nodo = node_load($nodo_id);
+$salida_html= '<div id="tabs">
           <ul>
             <li><a href="#tabs-1">'.$title_caracteristicas.'</a></li>
           </ul>
-          <div id="tabs-1">'.$desc_caracteristicas.'<a class="info cart-submit" href>'. t('Add to') .'</a></div>
+          <div id="tabs-1">'.$desc_caracteristicas;
+
+
+  if ($nodo->type=='cristales_de_vidrio_programados'){
+    $salida_html.= '<div class="btn-realizar-pedido lg-es"><a href="/pedido-de-cristales?producto='.$nodo->title.'" class="colorbox-node">'.t('Request this glass').'</a></div><div class="btn-realizar-pedido lg-en"><a href="/glass-request?product='.$nodo->title.'" class="colorbox-node">'.t('Request this glass').'</a></div></div></div>';
+  }else{
+    $salida_html.='<a class="info cart-submit" href>'. t('Add to') .'</a></div>
          </div>';
+  }
+
+
+echo $salida_html;
